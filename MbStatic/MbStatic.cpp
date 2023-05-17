389d593f15c246162c7c39d8e277450e14758a63
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-05-17 11:00:38 +0700

    menggunakan 5 variabel

--- MbStatic/MbStatic.cpp
+++ MbStatic/MbStatic.cpp
@@ -2,4 +2,14 @@
 #include <string>
 using namespace std;
 
-class mahasiswa {};
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
